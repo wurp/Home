ba12d14a31c43a22d2e51703caccfd055421c9b0
Bobby Martin <bobbymartin2@gmail.com>
2020-04-05 12:43:15 -0700

    put repos in ~/projects after pulling

--- init/pull-all-repos.sh
+++ init/pull-all-repos.sh
@@ -1,11 +1,26 @@
 set -e
 mydir=$(realpath $(dirname $0))
 
-cd $mydir
+cd $mydir/..
 mkdir -p build/local-repo-copies
 cd build/local-repo-copies
 
-for file in $(awk '{ print $1 }' "$mydir/git-repos.txt")
+mkdir -p ~/projects
+
+for gitrepo in $(awk '{ print $1 }' "$mydir/git-repos.txt")
 do
-  git clone $file
+  reponame=$(echo "$gitrepo" | sed 's#.*/\([^/]*\).git$#\1#')
+  if [ -e $reponame ]
+  then
+    echo "$reponame already pulled"
+  else
+    git clone $gitrepo
+  fi
+
+  if [ -e ~/projects/$reponame ]
+  then
+    echo "$reponame already in ~/projects"
+  else
+    ln -s $PWD/$reponame ~/projects/$reponame
+  fi
 done
